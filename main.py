d25c5a48be57669f6e241110f3b5e564677a4b0b
QuinnVN <96212613+QuinnVN@users.noreply.github.com>
2023-07-27 20:59:25 +0700

    fix small bugs/ refactor

--- main.py
+++ main.py
@@ -1,100 +1,105 @@
-from win11toast import notify
-from rss_parser import Parser
+import asyncio
+import datetime
+import json
+from time import sleep
+
 from requests import get
 from requests.exceptions import RequestException
-from time import sleep
-import json
-import datetime
+from rss_parser import Parser
+from win11toast import notify
+
 import schema
 
-def loadConfig ():
+
+async def load_config():
     try:
-        with open('config.json') as configFile:
+        with open('config.json', 'r') as configFile:
             config = json.load(configFile)
+
+            if not config['rssURL']:
+                notify(
+                    'RSS Notifier',
+                    'No RSS URL found',
+                    duration='short'
+                )
+
+        return config
     except:
-        print(config)
         notify(
-            'RSS Notificator',
-            'Cannot load Configuation',
-            duration = 'short'
+            'RSS Notifier',
+            'Cannot load Configuration',
+            duration='short'
         )
-        sleep(15*60)
-        Main()
-    
-    
-    if (len(config['rssURL']) == 0): 
-        notify(
-            'RSS Notificator',
-            'No RSS URL found',
-            duration = 'short'
-        )
-        sleep(15*60)
-        Main()
+        return None
 
-    return config
 
-def getData (config):
-    print(config)
+def get_data(config):
     try:
-        res = get(config['rssURL'], timeout=10)
+        res = get(config['rssURL'], timeout=30, headers={
+            "Accept": "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, /;q=0.8",
+            "User-Agent": "Chrome/51.0.2704.103"
+        })
+        if res.status_code == 404 or len(res.text) == 0:
+            notify(
+                'RSS Notifier',
+                'Data not found',
+                duration='short'
+            )
+            return None
+        print(res)
+        return res.text
     except RequestException:
         notify(
-            'RSS Notificator',
+            'RSS Notifier',
             'Request Failed',
-            duration = 'short'
+            duration='short'
 
         )
-        sleep(15*60)
-        Main()
-    if (res.status_code == 404 or len(res.text) == 0):
-        notify(
-            'RSS Notificator',
-            'Data not found',
-            duration = 'short'
-        )
-        sleep(15*60)
-        Main()
-    return res.text
+        return None
 
-     
-def ParseData(text: str):
+
+def parse_data(text: str):
     try:
         data = Parser.parse(text, schema=schema.DataSchema)
+        return data
     except:
         notify(
-            'RSS Notificator',
+            'RSS Notifier',
             'Cannot Parse Data',
-            duration = 'short'
+            duration='short'
         )
-        sleep(15*60)
-        Main()
-    return data
+        return None
 
-def Main():
-    config = loadConfig()
-    text = getData(config=config)
-    data = ParseData(text=text)
-    
-    for item in data: 
-        dataFile = open('./data.txt', 'wr')
-        lastPubdate = dataFile.read().split(':')[1]
-        itemDate = int(item.pubDate.split(' '))
-        day = int(itemDate[0].split('/')[1])
-        month = int(itemDate[0].split('/')[0])
-        year = int(itemDate[0].split('/')[2])
-        hour = int(itemDate[1].split(':')[0])
-        min = int(itemDate[1].split(':')[1])
-        sec = int(itemDate[1].split(':')[2])
-        itemDatetime = datetime.datetime(year, month, day, hour, min, sec)
-        if(itemDatetime > lastPubdate): 
-            dataFile.write(f'lastPubdate:{itemDatetime}')
-        dataFile.close()
 
-        notify(
-            'Thông báo từ trường',
-            item.title,
-            duration = 'short'
-        )
+async def main():
+    config = await load_config()
+    if not config: return
+    text = get_data(config=config)
+    if not text: return
+    data = parse_data(text=text)
+    if not data: return
+    data_file = open('./data.txt', 'r+')
+    for item in data:
+        raw_pubdate = data_file.read().split('>')
+        last_pubdate = datetime.datetime.strptime(raw_pubdate[1], '%Y-%m-%d %H:%M:%S')
+        item_date = datetime.datetime.strptime(item.pubDate, '%m/%d/%Y %H:%M:%S %p')
+        if item_date > last_pubdate:
+            data_file.write(f'lastPubdate>{item_date}')
+            notify(
+                'Thông báo từ trường',
+                item.title,
+                duration='short'
+            )
+        else:
+            notify(
+                'Thông báo từ trường',
+                'Không có thông báo mới',
+                duration='short'
+            )
+    data_file.close()
 
-Main()
-    
+
+while True:
+    asyncio.run(main())
+
+    sleep(60 * 15)
